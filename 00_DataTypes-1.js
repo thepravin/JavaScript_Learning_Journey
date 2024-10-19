9397bd634e1d61266e737b326e8697bc71940f0d
Pravin Nalawade <thepravin28@gmail.com>
2024-10-19 18:48:30 +0530

    files added

---
+++ 00_DataTypes-1.js
@@ -0,0 +1,70 @@
+/*
+1. Premitive 
+2. Non Premitive
+*/
+
+// 1. Premitive
+
+//  NaN ==> not a number
+
+// ---------------------- Number --------------------------
+// console.log("9: ", typeof 9);
+// console.log("9.5: ", typeof 9.5);
+// console.log("-9: ", typeof -9);
+
+//------------------------- string -------------------------
+
+// console.log("Hellow: ", typeof "Hellow");
+// console.log("Empty string: ", typeof "");
+// console.log('Hellow "Pravin"');
+// console.log("Hellow 'Pravin'");
+
+/*
+    ` ` = > backtick => support new line
+    ' ' => single cot => not support new line
+    
+ console.log(`Hi,
+ My name is pravin !!!`);
+
+ console.log('Hi,
+ I am Pravin ???');  error => SyntaxError: Invalid or unexpected token
+
+*/
+
+//--------------------- Str to int Conversion  -------------------------
+/*
+
+console.log(typeof '100')
+console.log(typeof +'100') // +ve 
+console.log(typeof -'100') // -ve 
+
+// string to number convert but have limitations
+console.log(+'100adfdf')
+console.log(parseInt('10adfdf'))
+
+// number to string convert
+console.log(typeof (''+ 100 + 'pravin') )
+console.log(( 100 + ' pravin') )
+
+*/
+
+//--------------------- Boolean  -------------------------
+
+// console.log(typeof true);
+// console.log(typeof (+true));
+// console.log(+ true);
+
+//------------------------- Undefined  -------------------------
+/* ====> create variable not assine value then present
+   ====> It have only one value which is "undefined"
+
+   console.log(+undefined); // NaN
+*/
+
+//------------------------- NULL -------------------------
+/*
+    ==> we assing it 
+    ==> type of NULL is 'nothing' but there is bug that is why type of null is "Object"
+   
+    console.log(+ null); // 0
+*/
---
+++ 01_Variables.js
@@ -0,0 +1,52 @@
+/*
+1. var
+2. const
+3. let
+
+==> in variable name can use only "$" and "_"
+==> we can use different language names  to give variable name
+==> name convention read --> https://www.freecodecamp.org/news/programming-naming-conventions-explained
+
+*/
+
+// ------------------- let -------------
+/*
+    -> typeof let is 'undefined' = Interaction with undeclared and uninitialized variables
+    -> we can change value of variable.
+    ->
+*/
+// ------------------- const -------------
+/*
+    -> typeof const  => give error unexpected tocken "must have to decrear at the time of initilazing"
+    -> we can not change value of variable.
+    ->
+*/
+// ------------------- var -------------
+/*
+    -> typeof var  => give error unexpected tocken "must have to decrear at the time of initilazing"
+    -> we can change value of variable.
+    ->
+*/
+
+// ------------------- var v/s let  -------------
+
+// Note :-> "Undefined is value" BUT "not defined is error"
+
+/*
+ 
+###### VAR ######
+    --> var will be accessiable outside the box or scope
+     {
+        var firstName = 'pravin';
+    }
+    console.log(firstName)
+
+    ---> we can access var befor initilazition it not give error but it give "undefined"
+   consolge.log(name);
+   var name = 'pravin';
+
+##### LET #######
+    ----> let not accessiable outside the box or scope
+        ---> give not defined error
+        ---> not access befor initilazition give error
+*/
