ff8a2f48f6029b7ee66aed1920a6a5bbd34229e0
Pravin Nalawade <thepravin28@gmail.com>
2024-11-24 13:49:50 +0530

    async-await

---
+++ 29_Async_Await.js
@@ -0,0 +1,20 @@
+/* 
+* Async *
+-> Async and Await in JavaScript is used to simplify handling asynchronous operations using promises.
+-> Async functions always return promise.f a value is returned that is not a promise, JavaScript automatically 
+   wraps it in a resolved promise.
+
+* Await *
+-> await used to wait for a promise to resolve.
+-> it can only be used within async block.
+-> await makes the code wait until promise returns result.
+
+* throw *
+=> throw used to reject the function.
+
+=> Syntax :
+        => throw 'error-message'
+        => throw new Error('error-message')
+
+
+****/
